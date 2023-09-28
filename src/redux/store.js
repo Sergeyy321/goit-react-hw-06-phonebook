2aeccbb9fd08cd58e12f9843ab4e04e4eda7e43a
Sergeyy321 <125265794+Sergeyy321@users.noreply.github.com>
2023-09-28 16:50:56 +0200

    Revert "jh"
    This reverts commit 12271df2edc5e5a0542cabef45502d82b89af772.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,22 +1,6 @@
-import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 
-import { devToolsEnhancer } from '@redux-devtools/extension';
-const initialState = {
-  tasks: [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ],
-  filters: {
-    filter: '',
-  },
-};
-
-
-const rootReducer = (state = initialState, action) => {
-  return state;
-};
-const enhancer = devToolsEnhancer();
-
-export const store = createStore(rootReducer);
+export const store = configureStore({
+    
+    reducer: {},
+})
