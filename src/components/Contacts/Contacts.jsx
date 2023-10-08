aca0af9158324815920ce7746de0d637486e58d1
Sergeyy321 <125265794+Sergeyy321@users.noreply.github.com>
2023-10-08 15:39:01 +0200

    fix three

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { ContactStyle } from './Contacts.styled';
-import { useSelector, useDispatch } from 'react-redux';
+import {  useDispatch } from 'react-redux';
 import { deleteContact } from 'Redux/sliceContact';
 
 export const Contacts = ({ getContacts }) => {
